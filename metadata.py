e1570bf24841bb6160c6c5b4bc06d9b07d5cbb7b
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-04-04 14:05:31 +0300

    Move the file reading functionality to seq_reader.py

--- metadata.py
+++ metadata.py
@@ -1,120 +1,28 @@
-import subprocess
-import sys
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-
-
-def seq_frames(filename):
-    block_size = 1024*1024 # 1MB
-
-    with open(filename, 'rb') as seq_file:
-        # read the file in blocks and split into frames at the magic pattern, "\x46\x46\x46\x00"
-        marker = "\x46\x46\x46\x00".encode()
-        frame = b''
-        index = 1
-        for block in iter(lambda: seq_file.read(block_size), ''):
-            frame += block
-            while True:
-                marker_position = frame.find(marker, len(marker))
-                if marker_position == -1:
-                    break
-                if marker_position == 0:
-                    frame = frame[len(marker):]
-                    continue
-                yield frame[:marker_position]
-                frame = frame[marker_position + len(marker):]
-                index += 1
-
+from seq_reader import extract_metadata, convert_to_temperature, save_image, seq_frames
 
 seq_file = 'Rec-000781.seq'
 
-# Use ExifTools to extract metadata
-metadata_str = subprocess.check_output(['exiftool', seq_file]).decode(sys.stdout.encoding)
-
-# create a dictionary to store the metadata values
-metadata = {}
-
-# split the metadata string into lines and iterate over them
-for line in metadata_str.split("\n"):
-    # skip any empty lines or lines that don't contain a colon separator
-    if not line or ":" not in line:
-        continue
-    
-    # split the line into a key-value pair using the colon separator
-    key, value = line.split(":", 1)
-    
-    # strip any whitespace from the key and value
-    key = key.strip()
-    value = value.strip()
-    
-    # store the key-value pair in the metadata dictionary
-    metadata[key] = value
-
-print(metadata)
-
+metadata = extract_metadata(seq_file)
 
 width = int(metadata["Raw Thermal Image Width"])
 height = int(metadata["Raw Thermal Image Height"])
 bitdepth = 16
-
-pr1 = float(metadata['Planck R1'])
-pr2 = float(metadata['Planck R2'])
-po = float(metadata['Planck O'])
-a1 = float(metadata['Atmospheric Trans Alpha 1'])
-a2 = float(metadata['Atmospheric Trans Alpha 2'])
-b1 = float( metadata['Atmospheric Trans Beta 1'])
-b2 = float( metadata['Atmospheric Trans Beta 2'])
-atx = float( metadata['Atmospheric Trans X'])
-e = float( metadata['Emissivity'])
-
-# Temperature readings, should be in C
-at, unit = metadata['Atmospheric Temperature'].split(' ')
-assert unit == 'C'
-at = float(at)
-rat, unit = metadata['Reflected Apparent Temperature'].split(' ')
-assert unit == 'C'
-rat = float(rat)
-    
-# Distance in m
-od, unit = metadata['Object Distance'].split(' ')
-assert unit == 'm'
-od = float(od)
-
-
-# Calculate the size of each frame in bytes
 frame_size = width * height * (bitdepth // 8)
 
+
 for frame_index, frame in enumerate(seq_frames(seq_file)):
+    raw_data = np.frombuffer(frame[len(frame)-frame_size:], dtype=np.uint16).reshape(height, width)
+    
+    save_image(raw_data, f'raw_data/{frame_index}.tiff')
 
-    image = np.frombuffer(frame[len(frame)-frame_size:], dtype=np.uint16).reshape(height, width)
-    img = Image.fromarray(image)
-    img.save(f'raw_data_{frame_index}.tiff')
+    temperature = convert_to_temperature(raw_data, metadata)
 
-    # Convert the raw data to temperature values
-    temperature_data = np.zeros_like(image, dtype=np.float32)
-    for i in range(height):
-        for j in range(width):
-            temperature_data[i][j] = pr1 / (pr2 * (image[i][j] - po))
-            temperature_data[i][j] -= a1 * at ** 3
-            temperature_data[i][j] -= a2 * at ** 2
-            temperature_data[i][j] += b1 * od ** 3
-            temperature_data[i][j] += b2 * od ** 2
-            temperature_data[i][j] += atx * od
-            temperature_data[i][j] -= rat
-            temperature_data[i][j] /= e
-        
-    # Create a PIL Image object from the pixel data
-    print(temperature_data[100:110,200])
-    temperature_data = (temperature_data/40.0*255).astype(np.uint8)
-    print(temperature_data[100:110,200])
-    img = Image.fromarray(temperature_data)
-        
-    # Save the image to a TIFF file
-    img.save(f'thermal_image_{frame_index}.tiff')
+    save_image(raw_data, f'thermal_image/{frame_index}.tiff')
 
-    #plt.imshow(temperature_data, vmin=temperature_data.min(), vmax=temperature_data.max())
-    #plt.show()
+
+
+    
 
 
 
--- seq_reader.py
+++ seq_reader.py
@@ -1,18 +1,7 @@
-from tqdm import tqdm
-import os
-from pathlib import Path
+import subprocess
+import sys
+import numpy as np
 from PIL import Image
-
-from flirpy.util.exiftool import Exiftool
-from flirpy.io.fff import Fff
-
-class Logger():
-    def info(self, message):
-        print(message)
-    def debug(self, message):
-        print(message)
-
-logger = Logger()
 
 
 def seq_frames(filename):
@@ -37,177 +26,86 @@ def seq_frames(filename):
                 index += 1
 
 
-class Splitter:
+def extract_metadata(seq_file):
+    # Use ExifTools to extract metadata
+    metadata_str = subprocess.check_output(['exiftool', seq_file]).decode(sys.stdout.encoding)
+
+    # create a dictionary to store the metadata values
+    metadata = {}
+
+    # split the metadata string into lines and iterate over them
+    for line in metadata_str.split("\n"):
+        # skip any empty lines or lines that don't contain a colon separator
+        if not line or ":" not in line:
+            continue
+
+        # split the line into a key-value pair using the colon separator
+        key, value = line.split(":", 1)
+
+        # strip any whitespace from the key and value
+        key = key.strip()
+        value = value.strip()
+
+        # store the key-value pair in the metadata dictionary
+        metadata[key] = value
+
+    return metadata
+
+def extract_temperature_images(seq_file, metadata, bitdepth = 16):
+    width = int(metadata["Raw Thermal Image Width"])
+    height = int(metadata["Raw Thermal Image Height"])
+
+    frame_size = width * height * (bitdepth // 8)
+
+    for frame_index, frame in enumerate(seq_frames(seq_file)):
+        raw_data = np.frombuffer(frame[len(frame)-frame_size:], dtype=np.uint16).reshape(height, width)
+        img = Image.fromarray(raw_data)
+        img.save(f'raw_data/{frame_index}.tiff')
+
+        temperature = convert_to_temperature(raw_data)
+
+
+
+def convert_to_temperature(frame, metadata):
+    planck_R1 = float(metadata['Planck R1'])
+    planck_R2 = float(metadata['Planck R2'])
+    planck_O = float(metadata['Planck O'])
+    Atmos_A1 = float(metadata['Atmospheric Trans Alpha 1'])
+    Atmos_A2 = float(metadata['Atmospheric Trans Alpha 2'])
+    Atmos_B1 = float(metadata['Atmospheric Trans Beta 1'])
+    Atmos_B2 = float(metadata['Atmospheric Trans Beta 2'])
+    Atmos_X = float(metadata['Atmospheric Trans X'])
+    emissivity = float(metadata['Emissivity'])
+
+    # Temperature readings, should be in C
+    Atmos_T, unit = metadata['Atmospheric Temperature'].split(' ')
+    assert unit == 'C'
+    Atmos_T = float(Atmos_T)
+    RA_TEMP, unit = metadata['Reflected Apparent Temperature'].split(' ')
+    assert unit == 'C'
+    RA_TEMP = float(RA_TEMP)
     
-    def __init__(self,
-                    output_folder="./output/",
-                    exiftool_path=None,
-                    start_index=0,
-                    step=1,
-                    split_folders=True,
-                    preview_format="jpg",
-                    width=None,
-                    height=None):
-        
-        self.exiftool = Exiftool(exiftool_path)
-            
-        self.start_index = start_index
-        self.step = step
-        self.frame_count = self.start_index
-        self.export_tiff = True
-        self.export_meta = True
-        self.export_preview = True
-        self.export_radiometric = True
-        self.overwrite = True
-        self.split_folders = split_folders
-        self.split_filetypes = True
-        self.width = width
-        self.height = height
-        
-        if preview_format in ["jpg", "jpeg", "png", "tiff"]:
-            self.preview_format = preview_format
-        else:
-            raise ValueError("Preview format not recognised")
+    # Distance in m
+    Object_dist, unit = metadata['Object Distance'].split(' ')
+    assert unit == 'm'
+    Object_dist = float(Object_dist)
 
-        self.output_folder = os.path.expanduser(output_folder)
-        Path(self.output_folder).mkdir(exist_ok=True)
-    
-    def set_start_index(self, index):
-        self.start_index = int(index)
-        
-    def process(self, file_list):
+    # Convert the raw data to temperature values
+    temperature_data = planck_R1 / (planck_R2 * (frame - planck_O))
+    temperature_data -= Atmos_A1 * Atmos_T ** 3
+    temperature_data -= Atmos_A2 * Atmos_T ** 2
+    temperature_data += Atmos_B1 * Object_dist ** 3
+    temperature_data += Atmos_B2 * Object_dist ** 2
+    temperature_data += Atmos_X * Object_dist
+    temperature_data -= RA_TEMP
+    temperature_data /= emissivity
 
-        if isinstance(file_list, str):
-            file_list = [file_list]
-
-        file_list = [os.path.expanduser(f) for f in file_list]
-
-        logger.info("Splitting {} files".format(len(file_list)))
-        
-        self.frame_count = self.start_index
-
-        folders = []
-        
-        for seq in tqdm(file_list):
-
-            if self.split_folders:
-                subfolder, _ = os.path.splitext(os.path.basename(seq))
-                folder = os.path.join(self.output_folder, subfolder)
-                folders.append(folder)
-            else:
-                folder = self.output_folder
-
-            Path(folder).mkdir(exist_ok=True)
-
-            logger.info("Splitting {} into {}".format(seq, folder))
-            self._process_seq(seq, folder)
-
-            # Batch export meta data
-            if self.export_meta:
-                logger.info("Extracting metadata")
-
-                if self.split_filetypes:
-                    filemask = os.path.join(folder, "raw", "frame_*.fff")
-                    copy_filemask = os.path.normpath("./raw/%f.fff")
-                    radiometric_folder = os.path.normpath("./radiometric")
-                    preview_folder = os.path.normpath("./preview")
-                else:
-                    filemask = os.path.join(folder, "frame_*.fff")
-                    copy_filemask = os.path.normpath("%f.fff")
-                    radiometric_folder = os.path.normpath("./")
-                    preview_folder = os.path.normpath("./")
-
-                self.exiftool.write_meta(filemask)
-
-            # Copy geotags
-            if self.export_tiff:
-                logger.info("Copying tags to radiometric")
-                self.exiftool.copy_meta(folder, filemask=copy_filemask, output_folder=radiometric_folder, ext="tiff")
-            
-            if self.export_preview:
-                logger.info("Copying tags to preview")
-                self.exiftool.copy_meta(folder, filemask=copy_filemask, output_folder=preview_folder, ext=self.preview_format)
-    
-        return folders
-        
-    def _write_tiff(self, filename, data):
-        logger.debug("Writing {}", filename)
-        Image.fromarray(data.astype("uint16")).save(filename)
-
-    def _write_preview(self, filename, data):
-        drange = data.max()-data.min()
-        preview_data = 255.0*((data-data.min())/drange)
-        logger.debug("Writing {}", filename)
-        Image.fromarray(preview_data.astype('uint8')).save(filename)
-            
-    def _make_split_folders(self, output_folder):
-        Path(os.path.join(output_folder, "raw")).mkdir(exist_ok=True)
-        Path(os.path.join(output_folder, "radiometric")).mkdir(exist_ok=True)
-        Path(os.path.join(output_folder, "preview")).mkdir(exist_ok=True)
-
-    def _check_overwrite(self, path):
-        exists = os.path.exists(path)
-        return (not exists) or (exists and self.overwrite)
-    
-    def _process_seq(self, input_file, output_subfolder):
-        
-        logger.debug("Processing {}".format(input_file))
-        
-        for count, frame in enumerate(tqdm(seq_frames(input_file))):
-            frame = Fff(frame, None, None)
-
-            if frame.meta is None:
-                self.frame_count += 1
-                continue
-                
-            if self.split_filetypes:
-                self._make_split_folders(output_subfolder)
-                                    
-                filename_fff = os.path.join(output_subfolder, "raw", "frame_{0:06d}.fff".format(self.frame_count))
-                filename_tiff = os.path.join(output_subfolder, "radiometric", "frame_{0:06d}.tiff".format(self.frame_count))
-                filename_preview = os.path.join(output_subfolder, "preview", "frame_{:06d}.{}".format(self.frame_count, self.preview_format))
-                filename_meta = os.path.join(output_subfolder, "raw", "frame_{0:06d}.txt".format(self.frame_count))
-            else:
-                filename_fff = os.path.join(output_subfolder, "frame_{0:06d}.fff".format(self.frame_count))
-                filename_tiff = os.path.join(output_subfolder, "frame_{0:06d}.tiff".format(self.frame_count))
-                filename_preview = os.path.join(output_subfolder, "frame_{:06d}.{}".format(self.frame_count, self.preview_format))
-                filename_meta = os.path.join(output_subfolder, "frame_{0:06d}.txt".format(self.frame_count))
-            
-            if self.frame_count % self.step == 0:
-
-                if self.export_meta and self._check_overwrite(filename_fff):
-                    frame.write(filename_fff)
-                
-                # Export raw files and/or radiometric convert them
-                if self.export_tiff and self._check_overwrite(filename_tiff):
-                    if self.export_radiometric:
-                        
-                        # Use Exiftool to extract metadata
-                        if self.width is not None and self.height is not None:
-                            # Export the first metadata
-                            if count == 0:
-                                self.exiftool.write_meta(filename_fff)
-                                meta = self.exiftool.meta_from_file(filename_meta)
-                        else:
-                            meta = None
-
-                        image = frame.get_radiometric_image(meta=meta)
-                        image += 273.15 # Convert to Kelvin
-                        image /= 0.04 # Standard FLIR scale factor
-                    else:
-                        image = frame.get_image()
-
-                    self._write_tiff(filename_tiff, image)
-
-                # Export preview frame (crushed to 8-bit)
-                if self.export_preview and self._check_overwrite(filename_preview):
-                    self._write_preview(filename_preview, image)
-        
-            self.frame_count += 1
-                    
-        return
+    return temperature_data
 
 
-splitter = Splitter()
-splitter.process("Rec-000781.seq")
+def save_image(data, filename, scaled=False):
+    if scaled:
+        data = (data/30.0*255).astype(np.uint8)
+    img = Image.fromarray(data)
+    img.save(filename)
 
