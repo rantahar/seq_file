128807285eefc694aa580a6bf3557f5522ae5369
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2023-04-26 14:45:37 +0300

    Track subjects and write out temperature

--- read_temperatures.py
+++ read_temperatures.py
@@ -18,7 +18,10 @@ bitdepth = 16
 frame_size = width * height * (bitdepth // 8)
 
 
+heads = []
+
 for frame_index, frame in enumerate(seq_frames(seq_file)):
+    print(frame_index, len(heads))
     raw_data = np.frombuffer(frame[len(frame)-frame_size:], dtype=np.uint16).reshape(height, width)
     
     #save_image(raw_data, f'raw_data/{frame_index}.png')
@@ -27,8 +30,36 @@ for frame_index, frame in enumerate(seq_frames(seq_file)):
 
     #save_image(temperature, f'thermal_image/{frame_index}.png', scaled=True)
 
-    heads = detect_heads(temperature)
-    print(heads)
+    frame_heads = detect_heads(temperature)
+    for frame_head in frame_heads:
+        matched = False
+        for i, head in enumerate(heads):
+            if head["width"] != frame_head["width"]:
+                continue
+            dx = head["x"] - frame_head["x"]
+            dy = head["y"] - frame_head["y"]
+            a = head["width"]/2
+            b = head["height"]/2
+            if (dx**2/a**2 + dy**2/b**2) < 0.05:
+                matched = True
+                id = heads[i]["subject_id"]
+                heads[i] = frame_head
+                heads[i]["subject_id"] = id
+                break
+        if not matched:
+            heads.append(frame_head)
+            
+    for i, head in enumerate(heads):
+        if "subject_id" not in head:
+            heads[i]["subject_id"] = i
+            save_head_image(temperature, head)
+            
+
+    df = pd.DataFrame(heads)
+    if frame_index == 0:
+        df.to_csv("temperatures.csv", index=False)
+    else:
+        df.to_csv("temperatures.csv", mode="a", index=False, header=False)
     
 
     
