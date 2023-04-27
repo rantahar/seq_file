792715c5597a11f8d255acbae4ce7a177bbcf55b
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2023-04-27 09:25:34 +0300

    larger subject image

--- utils.py
+++ utils.py
@@ -22,12 +22,16 @@ def get_rectangle(image, head, margin = 40):
     rect = image[ymin:ymax, xmin:xmax]
     return rect, (xmin, ymin)
 
-def save_head_image(image, head):
-    rect, _ = get_rectangle(image, head)
+def save_head_image(image, head, index=None):
+    rect, min_coord = get_rectangle(image, head, margin=80)
+    y, x = head["y"], head["x"]
     h = head["height"]//2
     w = head["width"]//2
-    cv2.ellipse(rect, (w+40, h+40), (w, h), 0, 0, 360, thickness=2, color=255)
-    save_image(rect, f'subjects/{head["subject_id"]}.png', scaled=True)
+    cv2.ellipse(rect, (x-min_coord[0], y-min_coord[1]), (w, h), 0, 0, 360, thickness=2, color=255)
+    filename = f'{head["subject_id"]}.png'
+    if index is not None:
+        filename = f"frame_{index}_" + filename
+    save_image(rect, "subjects/"+filename, scaled=True)
     gray = ((image - 10) / 40*255).astype('uint8')
     #cv2.imshow("gray", gray)
     #cv2.waitKey(0)
